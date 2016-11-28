da817398e6264de968604e14ffb28143dd49619b
ICAR ECOS research group <luca@iMac-di-Luca.local>
2016-11-28 11:04:12 +0100

    not using FreeBuilder by now

--- src/jcore/layer/awareness/AbstractCapability.java
+++ src/jcore/layer/awareness/AbstractCapability.java
@@ -8,22 +8,22 @@ import layer.semantic.Condition;
 import layer.semantic.evolution.AddStatement;
 import layer.semantic.evolution.EvolutionScenario;
 
-@FreeBuilder
-public interface AbstractCapability extends RunTimeEntity {
-//	private String unique_id;
-//	
-//	private Set<EvolutionScenario> evolution_set;
-//	private Condition pre;
-//	private Condition post;
-//	
-//	public AbstractCapability(String unique_id, Set<EvolutionScenario> evolution_set, Condition pre, Condition post) {
-//		super();
-//		this.unique_id = unique_id;
-//		this.evolution_set = evolution_set;
-//		this.pre = pre;
-//		this.post = post;
-//	}
-//	
+//@FreeBuilder
+public class AbstractCapability implements RunTimeEntity {
+	private String unique_id;
+	
+	private Set<EvolutionScenario> evolution_set;
+	private Condition pre;
+	private Condition post;
+	
+	public AbstractCapability(String unique_id, Set<EvolutionScenario> evolution_set, Condition pre, Condition post) {
+		super();
+		this.unique_id = unique_id;
+		this.evolution_set = evolution_set;
+		this.pre = pre;
+		this.post = post;
+	}
+	
 //	public void test_evo() {
 //		EvolutionScenario s = new EvolutionScenario.Builder()
 //				.setName("uno")
@@ -31,11 +31,11 @@ public interface AbstractCapability extends RunTimeEntity {
 //				.build();
 //	}
 
-	public String getName();
-	public Set<EvolutionScenario> getEvolutions();
-	public Condition getPreCondition();
-	public Condition getPostCondition();
-	
-	class Builder extends AbstractCapability_Builder {}
+//	public String getName();
+//	public Set<EvolutionScenario> getEvolutions();
+//	public Condition getPreCondition();
+//	public Condition getPostCondition();
+//	
+//	class Builder extends AbstractCapability_Builder {}
 	
 }
