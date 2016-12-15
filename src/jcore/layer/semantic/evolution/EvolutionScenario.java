ead31dba3cd1f70b6ec06bfdfeb09bc3c0c2002b
ICAR ECOS research group <luca@iMac-di-Luca.local>
2016-12-15 16:51:00 +0100

    removed FreeBuilder

--- src/jcore/layer/semantic/evolution/EvolutionScenario.java
+++ src/jcore/layer/semantic/evolution/EvolutionScenario.java
@@ -4,11 +4,8 @@ import java.util.Set;
 
 import org.inferred.freebuilder.FreeBuilder;
 
-@FreeBuilder
 public interface EvolutionScenario {
 	String getName();
 	Set<EvolutionOperator> getOperators();
 	
-	class Builder extends EvolutionScenario_Builder { }
-
 }
