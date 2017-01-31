2aedd42bf8a85f02ef398c6e9db5e445885c1c3e
miker83z <mikerz83@gmail.com>
2017-01-31 12:20:04 +0100

    sync2 31/1

--- src/jcore/layer/awareness/net/Token.java
+++ src/jcore/layer/awareness/net/Token.java
@@ -2,4 +2,13 @@ package layer.awareness.net;
 
 public class Token {
 	
+	private String placeName;
+		
+	public Token( String placeName ) {
+		this.placeName = placeName;
+	}
+	
+	public String getPlaceName() {
+		return placeName;
+	}
 }
