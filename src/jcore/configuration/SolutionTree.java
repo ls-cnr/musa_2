9c98fb88b7a6989193e48c24483ec19e91be1b76
mavnt <mirko.avantaggiato@gmail.com>
2017-03-10 16:16:06 +0100

    sync 10/3

--- src/jcore/configuration/SolutionTree.java
+++ src/jcore/configuration/SolutionTree.java
@@ -3,6 +3,7 @@ package configuration;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import pmr.graph.NormalEdge;
 import pmr.graph.OPNode;
 import pmr.graph.WTS;
 import pmr.graph.WorldNode;
@@ -14,9 +15,8 @@ import pmr.graph.WorldNode;
  * @author Mirko Avantaggiato
  *
  */
-
+@SuppressWarnings("unused")
 public class SolutionTree {
-
 
 	private TreeNode root;
 
@@ -32,12 +32,47 @@ public class SolutionTree {
 
 	/**
 	 * This method produces the lists of xor_nodes and failure_nodes
+	 * 
+	 * @param w0
+	 *            initial state
+	 * @param success_nodes
+	 *            success states
+	 * @param failure_nodes
+	 *            leaf node that aren't success nodes, found during the visit;
+	 * @param visited
+	 *            visited node in this DFS-like visit
+	 * @param xor_nodes
+	 *            xor_nodes found during the visit
 	 */
+	public void preliminaryVisit(WorldNode w0, HashMap<WorldNode, WorldNode> success_nodes,
+			HashMap<WorldNode, WorldNode> failure_nodes, HashMap<WorldNode, WorldNode> visited,
+			HashMap<WorldNode, ArrayList<OPNode>> xor_nodes) {
 
-	public void preliminaryVisit() {
-		/* TODO */
+		/*
+		 * If w0 has no outgoing edge and it's not contained in success_nodes,
+		 * it means it is a failure node, so it is put in the appropriate map.
+		 */
+		if (w0.getOutcomingEdgeList().isEmpty() && !success_nodes.containsKey(w0)) {
+			failure_nodes.put(w0, w0);
+			return;
+		}
+		visited.put(w0, w0);
+		xor_nodes.put(w0, w0.getOPNodeList());
+
+		for (NormalEdge normalEdge : w0.getOutcomingEdgeList()) {
+			if (!visited.containsKey(normalEdge)) {
+				preliminaryVisit(normalEdge.getDestination(), success_nodes, failure_nodes, visited, xor_nodes);
+			}
+		}
+		visited.remove(w0);
 	}
 
+	/**
+	 * @param wts
+	 *            the world transition system to work on
+	 * @param success_nodes
+	 *            success_nodes of the WTS
+	 */
 	public SolutionTree(WTS wts, HashMap<WorldNode, WorldNode> success_nodes) {
 		super();
 		this.wts = wts;
@@ -54,7 +89,7 @@ public class SolutionTree {
 	}
 
 	/**
-	 * This method produces the tree on which the real algorithm will operate
+	 * This method produces the tree the real algorithm will work on
 	 * and exctract the actual solution(s).
 	 */
 	public void pathList_toTree() {
@@ -67,5 +102,4 @@ public class SolutionTree {
 	public void tree_toSolutionSet() {
 		/* TODO */
 	}
-
 }
---
+++ src/testcase/configurationtest/SolutionTreeTest.java
@@ -0,0 +1,14 @@
+package configurationtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SolutionTreeTest {
+
+	@Test
+	public void test() {
+		fail();
+	}
+
+}
